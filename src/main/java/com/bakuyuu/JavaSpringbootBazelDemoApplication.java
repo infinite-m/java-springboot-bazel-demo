4e9f90f6d483538b7c874225d98467470a09179a
Jiuyu Ma <jiuyu.ma@compass.com>
2022-03-16 23:19:37 +0800

    master 3

--- src/main/java/com/bakuyuu/JavaSpringbootBazelDemoApplication.java
+++ src/main/java/com/bakuyuu/JavaSpringbootBazelDemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringbootBazelDemoApplication {
 
+	/**
+	 * master 3
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringbootBazelDemoApplication.class, args);
 	}
